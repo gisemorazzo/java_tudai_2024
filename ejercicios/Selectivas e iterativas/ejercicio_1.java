b0a580e579ddd78907351009587344774c5ebd30
gisele morazzo <97913498+gisemorazzo@users.noreply.github.com>
2024-04-14 15:29:25 -0300

    agrego nuevos cambios en carpetas

---
+++ ejercicios/Selectivas e iterativas/ejercicio_1.java
@@ -0,0 +1,18 @@
+
+//Escribir un programa completo (declaración de constantes y
+//variables, carga de datos, procesamiento e impresión de
+//resultados) para cada caso o ítem que:
+//////a. solicite un número e indique si es positivo o negativo.
+//b. solicite un número e imprima “grande” si es mayor a 100 y
+////“chico “ si es menor.
+////c. solicite un número del 1 al 7 e imprima el día de la semana.
+//d. solicite una letra e imprima si es vocal o consonante.
+///e. solicite 3 números e imprima “creciente” si los tres números
+//están en orden creciente, “decreciente” si están en orden
+//decreciente o “error” si no cumplen ningún orden
+
+public class ejercicio_1 {
+    public static void main(String[] args) {
+        
+    }
+}
